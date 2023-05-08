224f991f0fb7569efe7f1c28cf578b7d95b41eb1
Chandrika Thullimalli <thullimalli.chandrika0@gmail.com>
2023-05-08 21:16:57 +1000

    Add scripts to validate home loan calculator

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # HomeLoadCalculator
+
+This repo contains test scripts to validate the behavior of Home Loan Calculator.
+
+## Prerequisite
+latest node version installed.
+
+
+## Setup
+
+Run npm install to install all the dependencies.
+
+## Folder Structure
+
+cypress/e2e has the test scripts.
+
+cypress/fixtures contain test data.
+
+cypress/results contain the latest execution results.
+
+cypress/support contain custom commands.
+
+## Commands to execute tests
+
+npm run cy:run
+
+## Results
+
+cypress/results contains html and json files with the latest run results.
---
+++ cypress/e2e/HomeLoanCalculatorTests/HomeLoanCalculatorForm.cy.js
@@ -0,0 +1,49 @@
+//StoryNumber:0000 QA : Test scripts to validate the Home Loan Estimate and reset option
+describe('Home Loan Calculator App', () => {
+    beforeEach(() => {
+      cy.visit(`${Cypress.config("baseURL")}`);
+    });
+
+    it('fill the form and verify the borrowing estimate', () => {
+        cy.title().should("eq","Home loan borrowing power calculator | ANZ");
+        cy.contains("How much could I borrow?").should("be.visible");
+        //Fill Your Details
+        cy.fixture('HomeLoanEstimateData').then(function(regdata){
+            this.regdata=regdata;
+        cy.get(this.regdata.ApplicantType).click();
+        cy.get("[title = 'Number of dependants']").select(this.regdata.Dependants);
+        cy.get("#borrow_type_home").click();
+        cy.contains("Your annual income (before tax)").next('div').find('input').type(this.regdata.AnnualIncome);
+        cy.get("#q2q2i1").next('input').type(this.regdata.OtherAnnualIncome);
+        cy.get("#expenses").type(this.regdata.MonthlyLivingExpenses);
+        cy.get("#homeloans").type(this.regdata.Repayments);
+        cy.get("#otherloans").type(this.regdata.OtherRepayments);
+        cy.get("#q3q4i1").next('input').type(this.regdata.Commitments);
+        cy.get("#credit").type(this.regdata.CreditCardLimit);
+        cy.get("#btnBorrowCalculater").click().then(()=>{
+            cy.get("#borrowResultTextAmount").invoke('text').should('eq',this.regdata.Estimate);
+            cy.get(".start-over").should("be.visible");
+        });
+        });
+    });
+
+    it('start over and validate field reset', () => {
+        cy.get(".start-over").click({multiple:true,force: true},()=>{
+            cy.get("#application_type_single :checked").should('be.checked');
+            cy.get("#application_type_joint").should('not.be.checked');
+            cy.get("#borrow_type_home :checked").should('be.checked');
+            cy.get("#borrow_type_investment").should('not.be.checked');
+            cy.get("#q2q1i1").next('input').invoke("value").eq(0);
+            cy.get("#q2q2i1").next('input').invoke("value").eq(0);
+            cy.get("#expenses").invoke("value").eq(0);
+            cy.get("#homeloans").invoke("value").eq(0);
+            cy.get("#otherloans").invoke("value").eq(0);
+            cy.get("#q3q4i1").next('input').invoke("value").eq(0);
+            cy.get("#credit").invoke("value").eq(0);
+            cy.get("#borrowResultTextAmount").invoke('text').should('eq','0');
+
+        });
+
+    });
+
+})
